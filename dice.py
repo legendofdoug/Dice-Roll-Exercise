48bd2a9023583a4e5a84415b52a3fccb4c4e116b
doug <douglas.dvn@gmail.com>
2019-07-17 22:58:00 -0700

    added Error handling and case insensitivity

--- dice.py
+++ dice.py
@@ -1,8 +1,10 @@
 import random
 check = "yes"
-while check != "no":
+while check.lower() != "no":
     print ("Rolling this bitch!")
     rolled_num =  random.randint(1,6)
     print ("You rolled a", rolled_num)
     check = input("Would you like to roll again? yes/no \n")
-    
+    while check.lower() != "no" and check.lower() != "yes":
+        print("Please type 'yes' or 'no'.")
+        check = input("Would you like to roll again? yes/no \n")
